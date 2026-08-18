f40e3fa336ceeed3cb8aeaeb61a19028c2bcd941
vinn-o <218386044+vinn-o@users.noreply.github.com>
2026-08-18 18:18:06 +0300

    new variables with the function get_stock()

--- y_finance2.py
+++ y_finance2.py
@@ -3,13 +3,21 @@ import datetime
 from datetime import date, datetime, timezone
 
 def main():
-    ticker = "VT"
+
     start = datetime(2023, 1, 1)
     end   = datetime.today()
 
-    data = get_stock(ticker, start, end)
-    data = data.pivot(index="Date", columns="Ticker", values="Close")
-    print(data.head())
+    # data = get_stock("SPY", start, end)
+    # data = data.pivot(index="Date", columns="Ticker", values="Close")
+    # print(data.head())
+
+    SPY = get_stock("SPY", start, end)
+    IYW= get_stock("IYW", start, end)
+    VT = get_stock("VT", start, end)
+    DBA = get_stock("DBA", start, end)
+    TLT = get_stock("TLT", start, end)
+    PDBC = get_stock("PDBC", start, end)
+    IAU = get_stock("IAU", start, end)
 
 
 if __name__ == "__main__":
