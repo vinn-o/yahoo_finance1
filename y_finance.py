3ee1907dd1695458a1184b52f89d7aed0a9f571e
vinn-o <218386044+vinn-o@users.noreply.github.com>
2026-08-18 17:23:28 +0300

    Used the IYW ticker

--- y_finance.py
+++ y_finance.py
@@ -1,10 +1,17 @@
 import pandas as pd
 import y_finance as yf
+import numpy as np
 import datetime
 from datetime import time, datetime, timezone
 
-def get_stock():
+def get_stock(ticker, start, end):
+    pass
 def main():
+    start = datetime(2026, 1, 1)
+    end = datetime.today()
+    ticker = "IYW" # IYW track U.S technology company market capitalization index
+    d = get_stock(ticker, start, end)
+
     
 
 
