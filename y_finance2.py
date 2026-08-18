aa0afa5490b91b170c09ba487d3128be61baced5
vinn-o <218386044+vinn-o@users.noreply.github.com>
2026-08-18 18:14:18 +0300

    tested pivot for one data series

--- y_finance2.py
+++ y_finance2.py
@@ -1,13 +1,15 @@
-import y_finance
+from y_finance import get_stock
 import datetime 
 from datetime import date, datetime, timezone
+
 def main():
     ticker = "VT"
     start = datetime(2023, 1, 1)
     end   = datetime.today()
 
-    data = y_finance.get_stock(ticker, start, end)
-    print(data)
+    data = get_stock(ticker, start, end)
+    data = data.pivot(index="Date", columns="Ticker", values="Close")
+    print(data.head())
 
 
 if __name__ == "__main__":
