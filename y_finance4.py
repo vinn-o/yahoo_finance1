9986b8645fddfc31924f6c019d99136873a4c078
vinn-o <218386044+vinn-o@users.noreply.github.com>
2026-08-19 09:35:25 +0300

    new file to reduce lines

---
+++ y_finance4.py
@@ -0,0 +1,17 @@
+from y_finance import get_stock
+import datetime 
+import pandas as pd
+from datetime import date, datetime, timezone
+
+
+def main():
+    start = datetime(2024, 1,1)
+    end = datetime.today()
+
+    tickers = ["SPY", "IYW", "VT", "DBA", "TLT", "PDBC", "IAU"]
+
+    data
+
+
+if __name__ == "__main__":
+    main()
