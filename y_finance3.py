706f1a59d4095debcb80a7c53b90172da6fa6910
vinn-o <218386044+vinn-o@users.noreply.github.com>
2026-08-19 09:22:46 +0300

    list for all the tickers and an empty dictionary for adding the individual tickers

---
+++ y_finance3.py
@@ -0,0 +1,19 @@
+from y_finance import get_stock
+import datetime 
+import pandas as pd
+from datetime import date, datetime, timezone
+
+
+def main():
+    start = datetime(2025, 1, 1)
+    end = datetime.today()
+
+    tickers = ["SPY", "IYW", "VT", "DBA", "TLT", "PDBC", "IAU"]  #all tickers in one list for DRY
+    data = {}
+
+
+
+
+
+if __name__ == "__main__":
+    main()
