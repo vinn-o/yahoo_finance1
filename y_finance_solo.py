971e5e2cf7139b57a80d309da8a69568894cf672
vinn-o <218386044+vinn-o@users.noreply.github.com>
2026-08-21 12:10:15 +0300

    if instance() to check if the data column is of the pandas multiIndex format

--- y_finance_solo.py
+++ y_finance_solo.py
@@ -5,4 +5,7 @@ import matplotlib.pyplot as plt
 
 def get_stock(ticker, start, end):
     data = yf.download(ticker, start=start, end=end, auto_adjust=False)
+    if isinstance(data.columns, pd.MultiIndex):
+        data.columns = data.columns.get_value_levels(0)
+        
     
