c74ce0f08ba04e7894cac9ceec9a92ad4c3de38a
vinn-o <218386044+vinn-o@users.noreply.github.com>
2026-08-21 12:44:39 +0300

    migrate the  library addition and pd.concat to a function

--- y_finance_solo.py
+++ y_finance_solo.py
@@ -2,6 +2,7 @@ import yfinance as yf
 import pandas as pd
 import matplotlib.pyplot as plt
 import datetime
+from datetime import datetime
 
 
 
@@ -14,20 +15,24 @@ def get_stock(ticker, start, end):
     data["Tickers"] = ticker  #for new column called Tickers
     return data
 
-def main():
-    start = datetime.datetime(2018, 1,1)
-    end = datetime.datetime.today()
-    tickers = ["SPY", "VT", "IYW", "TLT", "DBA", "PDBC", "IAU"] #all the tickers
-
-    data = {} # empty dict that 
-
+def concata(tickers, start, end):
+    tickers = tickers
+    data = {}
     for i in tickers:
         data[i] = get_stock(i, start, end).pivot(index="Date", columns="Tickers", values="Close")  # one loop for DRY,
-
-        # tickers are added to each i in get_stock and placed in the library
+    
+            # tickers are added to each i in get_stock and placed in the library
     s = pd.concat([data[i] for i in tickers],
-                   axis=1,
-                   join="outer")
+                       axis=1,
+                       join="outer")
+    return s
+
+def main():
+    start = datetime(2018, 1,1)
+    end = datetime.today()
+    tickers = ["SPY", "VT", "IYW", "TLT", "DBA", "PDBC", "IAU"] #all the tickers
+
+    s = concata(tickers, start, end)
     print(s.head())
 
 
