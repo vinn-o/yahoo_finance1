0f9240c3d2da1228db09d5aab8bdb53e08448d62
vinn-o <218386044+vinn-o@users.noreply.github.com>
2026-08-21 22:59:16 +0300

    bar graph  module with the graphs pngs

--- y_finance_solo.py
+++ y_finance_solo.py
@@ -15,25 +15,49 @@ def get_stock(ticker, start, end):
     data["Tickers"] = ticker  #for new column called Tickers
     return data
 
-def concata(tickers, start, end):
-    tickers = tickers
+def concator(tickers, start, end, value1):
     data = {}
     for i in tickers:
-        data[i] = get_stock(i, start, end).pivot(index="Date", columns="Tickers", values="Close")  # one loop for DRY,
+        data[i] = get_stock(i, start, end).pivot(index="Date", columns="Tickers", values=value1)  # one loop for DRY,
     
             # tickers are added to each i in get_stock and placed in the library
     s = pd.concat([data[i] for i in tickers],
-                       axis=1,
-                       join="outer")
+                    axis=1,
+                    join="outer")
     return s
+
+
+def line_graph(s): #matplot function to plot a line graph
+    plt.style.use("ggplot")
+    s.plot(figsize=(30,20))
+    plt.savefig("myplot5.png")
+    plt.show()
+
+
+# def bar_graph(s):
+#     x = s.index                                     #  <<<<<<<<<<<<<<<<<<<<<    revisit   >>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>
+#     y = s
+#     plt.figure(figsize=(30, 20))
+#     plt.bar(x, y)
+#     plt.savefig("barplot.png")
+#     plt.show()
+
+
 
 def main():
     start = datetime(2018, 1,1)
     end = datetime.today()
     tickers = ["SPY", "VT", "IYW", "TLT", "DBA", "PDBC", "IAU"] #all the tickers
+    value1 = "Close"
 
-    s = concata(tickers, start, end)
-    print(s.head())
+
+
+   
+    merged = concator(tickers, start, end, value1)
+    # df = merged.drop("")
+    print(merged.head())
+    line_graph(merged)
+    # bar_graph(merged)
 
 
 
