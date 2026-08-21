ca81338175b00b9f5f8f1ab3491c1a42b3b0c634
vinn-o <218386044+vinn-o@users.noreply.github.com>
2026-08-21 12:24:23 +0300

    converted .get_value_levels()  to .get_level_values()

--- y_finance_solo.py
+++ y_finance_solo.py
@@ -8,7 +8,7 @@ import datetime
 def get_stock(ticker, start, end):
     data = yf.download(ticker, start=start, end=end, auto_adjust=False) #auto_adjust to not adjust the closing and have column for the adjust
     if isinstance(data.columns, pd.MultiIndex):
-        data.columns = data.columns.get_value_levels(0)
+        data.columns = data.columns.get_level_values(0)
     data = data.loc[:, ~data.columns.duplicated()]
     data = data.reset_index()   #to reset index from dates to numbers
     data["Tickers"] = ticker  #for new column called Tickers
@@ -17,12 +17,13 @@ def get_stock(ticker, start, end):
 def main():
     start = datetime.datetime(2018, 1,1)
     end = datetime.datetime.today()
-    tickers = ["SPY", "VT", "IYW", "TLT", "DBA", "PDBC", "IAU"]
+    # tickers = ["SPY", "VT", "IYW", "TLT", "DBA", "PDBC", "IAU"] #all the tickers
+    dd = get_stock("SPY", start, end)
+    print(dd.head())
+    # data = {} # empty dict that 
 
-    data = {}
-
-    for i in tickers:
-        data[i] = 
+    # for i in tickers:
+    #     data[i] = get_stock(i, start, end).pivot(index="Date")
 
 
 
